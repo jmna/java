e3886296817096f13fc1170d0c0e8861568eb139
jmna <jmnalves@gmail.com>
2014-09-23 10:45:07 +0100

    First Commit

---
+++ Project1/src/com/galeriajoalves/github/Project1.java
@@ -0,0 +1,10 @@
+package com.galeriajoalves.github;
+
+public class Project1 {
+	final static String message="Hello World!!";
+
+	public static void main(String[] args) {
+		System.out.println(message);
+	}
+
+}
