9c165d2457f4c187513827efa520d104b0be18e9
jmna <jmnalves@gmail.com>
2014-09-23 11:06:29 +0100

    Third Commit

--- Project1/src/com/galeriajoalves/github/Project1.java
+++ Project1/src/com/galeriajoalves/github/Project1.java
@@ -1,7 +1,7 @@
 package com.galeriajoalves.github;
 
 public class Project1 {
-	final static String message="Hello World!";
+	final static String message="Hello World2!";
 
 	public static void main(String[] args) {
 		System.out.println(message);
